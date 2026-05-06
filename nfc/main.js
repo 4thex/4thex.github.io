d0755c028031f1ebb628c1aa11c3d3055790acaf
palle@4thex.com <palle@4thex.com>
2026-05-06 06:36:49 +0200

    Scan for tags

--- nfc/main.js
+++ nfc/main.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+if(!NDEFReader) {
+    console.error('No NFC Web API support');
+    return;
+}
+
+const ndef = new NDEFReader();
+const reader = await ndef.scan();
+console.log('Scan started');
