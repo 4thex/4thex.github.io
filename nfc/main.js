783fb0b5e9d94f1ae804e733dd29f8ef9f0d4778
palle@4thex.com <palle@4thex.com>
2026-05-06 06:49:43 +0200

    User event trigger scan

--- nfc/main.js
+++ nfc/main.js
@@ -1,7 +1,10 @@
-if(!NDEFReader) {
-    console.error('No Web NFC API support');
-}
+const scan = () => {
+    if(!NDEFReader) {
+        console.error('No Web NFC API support');
+    }
 
-const ndef = new NDEFReader();
-const reader = await ndef.scan();
-console.log('Scan started');
+    const ndef = new NDEFReader();
+    const reader = await ndef.scan();
+    console.log('Scan started');
+};
+export {scan};
