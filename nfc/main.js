5babbf1cb93c6ac8c1475fd4d92a5560a020b38f
palle@4thex.com <palle@4thex.com>
2026-05-06 07:30:13 +0200

    Import styles and make button click event better

--- nfc/main.js
+++ nfc/main.js
@@ -1,7 +1,12 @@
+import styles from 'styles.css' with { type: 'css' };
+document.adoptedStyleSheets = [ styles ];
 const scanButton = document.querySelector('#scanButton');
-scanButton.addEventListener('click', async () => {
+// scanButton.addEventListener('click', async () => {
+//     await scan();
+// });
+scanButton.onclick = async () => {
     await scan();
-});
+};
 const scan = async () => {
     if(!NDEFReader) {
         console.error('No Web NFC API support');
@@ -10,4 +15,5 @@ const scan = async () => {
     const ndef = new NDEFReader();
     const reader = await ndef.scan();
     console.log('Scan started');
+
 };
